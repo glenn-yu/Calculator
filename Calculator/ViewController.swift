7d1748de2f958b5ae6586ca630092f9c2a9a5b7f
YUGWANGYONG <Yong@YUGWANGYONGs-MacBook-Pro.local>
2016-02-25 21:23:55 +0900

    second lesson, auto layout

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -29,9 +29,51 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBAction func enter() {
-        userIsInTheMiddleOfTypingANumber = false
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        switch operation {
+            case "×": performOperation{ $0 * $1 }
+            case "÷": performOperation{ $0 / $1 }
+            case "+": performOperation{ $0 + $1 }
+            case "−": performOperation{ $0 - $1 }
+            case "√": performOperation1{ sqrt($0) }
+        default: break
+        }
     }
     
+    func performOperation(operation: (Double, Double) -> Double){
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+
+    func performOperation1(operation: Double -> Double){
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+
+    var operandStack = Array<Double>()
+    
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        operandStack.append(displayValue)
+        print("operandStack = \(operandStack)")
+    }
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = false
+        }
+    }
 }
 
